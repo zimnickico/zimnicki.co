cb40433f27af76b1b1400ea898a6a433c0eed278
zimnickico <hi+github@krzysztofzimnicki.com>
2025-08-13 19:59:13 +0200

    microformats

--- ui/microformats.tsx
+++ ui/microformats.tsx
@@ -1,13 +1,12 @@
 export function Microformats() {
   return (
     <div className="h-card hidden">
-      <a className="u-url" rel="me" href="https://zimnicki.co">
-        <img className="u-photo" src="/me.jpg" alt="Christoph Zimnicki" />
-        <h2 className="p-photo">Christoph Zimnicki</h2>
-        <div className="p-note">
-          I{"'"}m an entrepreneur working across design, product and business.
-        </div>
-      </a>
+      <a className="u-url" rel="me" href="https://zimnicki.co"></a>
+      <img className="u-photo" src="/me.jpg" alt="Christoph Zimnicki" />
+      <h2 className="p-photo">Christoph Zimnicki</h2>
+      <div className="p-note">
+        I{"'"}m an entrepreneur working across design, product and business.
+      </div>
     </div>
   );
 }
