a38a6e995ad9409e5f5aa91f757a7e1f1d066136
zimnickico <hi+github@krzysztofzimnicki.com>
2025-08-13 19:46:12 +0200

    new

--- app/page.tsx
+++ app/page.tsx
@@ -32,6 +32,30 @@ export default function Page() {
         </WorkSample>
       </section>
       <Navigation />
+      <section className="mx-auto max-w-[42em]" id="webrings">
+        <p className="text-stone-400 text-sm font-[450] tracking-[-3%] leading-[120%] pt-[2em]">
+          Webrings
+        </p>
+        <div className="flex justify-between text-sm font-[450] tracking-[-3%] leading-[120%] pt-[2em] pb-[6em]">
+          <div>
+            An <a href="https://xn--sr8hvo.ws">IndieWeb Webring</a> 🕸💍
+          </div>
+          <div className="flex gap-[2em]">
+            <a
+              className="text-stone-400 hover:text-black transition-all duration-200"
+              href="https://xn--sr8hvo.ws/previous"
+            >
+              ←
+            </a>
+            <a
+              className="text-stone-400 hover:text-black transition-all duration-200"
+              href="https://xn--sr8hvo.ws/next"
+            >
+              →
+            </a>
+          </div>
+        </div>
+      </section>
       <section className="mx-auto max-w-[42em]" id="footer">
         <footer className="pb-[8em] flex items-center text-stone-400 text-sm font-[450] tracking-[-3%] leading-[120%] justify-between">
           <p>Last Updated: August 2025 </p>
